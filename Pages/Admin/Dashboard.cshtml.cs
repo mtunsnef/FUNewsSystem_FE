1be5ec7970e8d0c7ef02141e1de51222da42cf30
mtunsnef <tuannm23.dev@gmail.com>
2025-06-22 01:48:28 +0700

    style: add dashboard for admin

---
+++ Pages/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUNewsSystem.FE.Pages.Admin
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
