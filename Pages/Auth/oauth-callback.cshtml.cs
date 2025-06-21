52f139996c4bcdc29fc3b2647bded584e22cb70d
mtunsnef <tuannm23.dev@gmail.com>
2025-06-22 01:47:11 +0700

    style: add ui auth

---
+++ Pages/Auth/oauth-callback.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUNewsSystem.FE.Pages.Auth
+{
+    public class oauth_callbackModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
