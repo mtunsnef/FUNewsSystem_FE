db41350e0e12f7e8bf2e4e0db8f266e6a3284ff0
mtunsnef <tuannm23.dev@gmail.com>
2025-06-24 21:37:21 +0700

    style: add changepassword page

---
+++ Pages/Client/ChangePassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUNewsSystem.FE.Pages.Client
+{
+    public class ChangePasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
