8f85f2b8e1bbb2fd84ad6dc92e939609053d963a
mtunsnef <tuannm23.dev@gmail.com>
2025-06-24 21:32:57 +0700

    style: add category page

---
+++ Pages/Client/Category.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUNewsSystem.FE.Pages.Client
+{
+    public class CategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
